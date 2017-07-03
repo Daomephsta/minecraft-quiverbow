92e37fe1db85ed515c9917988f9f80d3f1681c7f
Leviathan143 <Leviathan143@users.noreply.github.com>
2017-07-03 22:00:08 +1200

    Nuke model tools. They aren't ready for publication

--- Model Tools/Ammo/CreateAmmoJSONs.bat
+++
@@ -1,2 +0,0 @@
-cd .\item\ammo
-python "C:\Users\David\Documents\Modding Resources\Modelling\GenBasicItemJSON.py" quiverchevsky arrow_bundle,rocket_bundle,large_rocket,cold_iron_clip,box_of_flint_dust ammo/
--- Model Tools/GenAllModels.bat
+++
@@ -1,5 +0,0 @@
-set startDir=%CD%
-cd ..\src\main\resources\assets\quiverchevsky\models
-CALL "%startDir%\Ammo\CreateAmmoJSONs.bat"
-cd %startDir%
-pause
