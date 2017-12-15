b506d8033cf928ff713b47a9bb3b4e7c0e392e45
Leviathan143 <Leviathan143@users.noreply.github.com>
2017-12-15 22:50:05 +1300

    Fix seedling

--- src/main/java/com/domochevsky/quiverbow/weapons/Seedling.java
+++ src/main/java/com/domochevsky/quiverbow/weapons/Seedling.java
@@ -21,82 +21,86 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class Seedling extends _WeaponBase
 {    
-    private int Dmg;
+	private int Dmg;
 
-    public Seedling()
-    {
-	super("seedling", 32);
-	setFiringBehaviour(new SingleShotFiringBehaviour<Seedling>(this, (world, weaponStack, entity, data) ->
+	public Seedling()
 	{
-	    float spreadHor = world.rand.nextFloat() * 10 - 5; // Spread
-	    float spreadVert = world.rand.nextFloat() * 10 - 5;
+		super("seedling", 32);
+		setFiringBehaviour(new SingleShotFiringBehaviour<Seedling>(this, (world, weaponStack, entity, data) ->
+		{
+			float spreadHor = world.rand.nextFloat() * 10 - 5; // Spread
+			float spreadVert = world.rand.nextFloat() * 10 - 5;
 
-	    Seed shot = new Seed(world, entity, (float) this.Speed, spreadHor, spreadVert);
-	    shot.damage = this.Dmg;
+			Seed shot = new Seed(world, entity, (float) this.Speed, spreadHor, spreadVert);
+			shot.damage = this.Dmg;
 
-	    return shot;
-	})
-	{
-	    @Override
-	    public void fire(ItemStack stack, World world, EntityLivingBase entity, EnumHand hand) { if(stack.getItemDamage() >= stack.getMaxDamage()) weapon.breakWeapon(world, stack, entity); }
-	});
-    }
-
-    // All ammo has been used up, so breaking now
-    private void breakWeapon(World world, ItemStack stack, Entity entity)
-    {
-	if (!(entity instanceof EntityPlayer)) // For QuiverMobs/Arms Assistants
-	{
-	    this.setCooldown(stack, 40);
-	    return;
+			return shot;
+		})
+		{
+			@Override
+			public void fire(ItemStack stack, World world, EntityLivingBase entity, EnumHand hand) 
+			{ 
+				super.fire(stack, world, entity, hand);
+				if(stack.getItemDamage() >= stack.getMaxDamage()) weapon.breakWeapon(world, stack, entity); 
+			}
+		});
 	}
 
-	EntityPlayer player = (EntityPlayer) entity;
-
-	player.renderBrokenItemStack(stack);
-	player.inventory.deleteStack(stack);
-	stack.setCount(0);
-
-	if(!world.isRemote)
+	// All ammo has been used up, so breaking now
+	private void breakWeapon(World world, ItemStack stack, Entity entity)
 	{
-	    entity.entityDropItem(new ItemStack(Blocks.PISTON), 1.0F);
-	    entity.entityDropItem(new ItemStack(Blocks.TRIPWIRE_HOOK), 1.0F);
+		if (!(entity instanceof EntityPlayer)) // For QuiverMobs/Arms Assistants
+		{
+			this.setCooldown(stack, 40);
+			return;
+		}
+
+		EntityPlayer player = (EntityPlayer) entity;
+
+		player.renderBrokenItemStack(stack);
+		player.inventory.deleteStack(stack);
+		stack.setCount(0);
+
+		if(!world.isRemote)
+		{
+			entity.entityDropItem(new ItemStack(Blocks.PISTON), 1.0F);
+			entity.entityDropItem(new ItemStack(Blocks.TRIPWIRE_HOOK), 1.0F);
+		}
+
+		Helper.playSoundAtEntityPos(player, SoundEvents.ENTITY_ITEM_BREAK, 1.0F, 1.5F);
 	}
 
-	Helper.playSoundAtEntityPos(player, SoundEvents.ENTITY_ITEM_BREAK, 1.0F, 1.5F);
-    }
-
-    @Override
-    public void doFireFX(World world, Entity entity)
-    {
-	entity.playSound(SoundEvents.BLOCK_WOOD_BUTTON_CLICK_ON, 0.6F, 0.7F);
-    }
-
-    @Override
-    public void addProps(FMLPreInitializationEvent event, Configuration config)
-    {
-	this.Enabled = config.get(this.name, "Am I enabled? (default true)", true).getBoolean(true);
-
-	this.Dmg = config.get(this.name, "What damage am I dealing per projectile? (default 1)", 1).getInt();
-
-	this.Speed = config.get(this.name, "How fast are my projectiles? (default 1.3 BPT (Blocks Per Tick))", 1.3)
-		.getDouble();
-
-	this.isMobUsable = config.get(this.name, "Can I be used by QuiverMobs? (default true)", true).getBoolean(true);
-    }
-
-    @Override
-    public void addRecipes()
-    {
-	if (this.Enabled)
+	@Override
+	public void doFireFX(World world, Entity entity)
 	{
-	    // One Seedling (fully loaded, meaning 0 damage)
-	    GameRegistry.addRecipe(new ItemStack(this, 1, 0), "ada", "ada", "bca", 'a', Items.REEDS, 'b',
-		    Blocks.TRIPWIRE_HOOK, 'c', Blocks.PISTON, 'd', Blocks.MELON_BLOCK);
+		entity.playSound(SoundEvents.BLOCK_WOOD_BUTTON_CLICK_ON, 0.6F, 0.7F);
 	}
-	else if (Main.noCreative)
+
+	@Override
+	public void addProps(FMLPreInitializationEvent event, Configuration config)
 	{
-	    this.setCreativeTab(null);
-	} // Not enabled and not allowed to be in the creative menu
-    }
+		this.Enabled = config.get(this.name, "Am I enabled? (default true)", true).getBoolean(true);
+
+		this.Dmg = config.get(this.name, "What damage am I dealing per projectile? (default 1)", 1).getInt();
+
+		this.Speed = config.get(this.name, "How fast are my projectiles? (default 1.3 BPT (Blocks Per Tick))", 1.3)
+				.getDouble();
+
+		this.isMobUsable = config.get(this.name, "Can I be used by QuiverMobs? (default true)", true).getBoolean(true);
+	}
+
+	@Override
+	public void addRecipes()
+	{
+		if (this.Enabled)
+		{
+			// One Seedling (fully loaded, meaning 0 damage)
+			GameRegistry.addRecipe(new ItemStack(this, 1, 0), "ada", "ada", "bca", 'a', Items.REEDS, 'b',
+					Blocks.TRIPWIRE_HOOK, 'c', Blocks.PISTON, 'd', Blocks.MELON_BLOCK);
+		}
+		else if (Main.noCreative)
+		{
+			this.setCreativeTab(null);
+		} // Not enabled and not allowed to be in the creative menu
+	}
 }
