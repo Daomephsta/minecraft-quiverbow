8a86c6b9fcbfe485819dea63eef3708d88c5c0b8
Leviathan143 <Leviathan143@users.noreply.github.com>
2018-06-20 19:29:47 +1200

    Fix stupid particle NPE

--- src/main/java/com/domochevsky/quiverbow/net/ParticleMessageHandler.java
+++ src/main/java/com/domochevsky/quiverbow/net/ParticleMessageHandler.java
@@ -3,6 +3,7 @@ package com.domochevsky.quiverbow.net;
 import com.domochevsky.quiverbow.HelperClient;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.relauncher.Side;
@@ -12,7 +13,12 @@ public class ParticleMessageHandler implements ISidedMessageHandler<ParticleMess
 	@Override
 	public void processMessage(ParticleMessage message, MessageContext ctx)
 	{
-		HelperClient.displayParticles(Minecraft.getMinecraft().world.getEntityByID(message.entityID),
+		Entity entity = Minecraft.getMinecraft().world.getEntityByID(message.entityID);
+		/* It's possible for the entity to be null on the client world if it
+		 * moves out of the client's loading range after this packet has been
+		 * sent */
+		if(entity == null) return;
+		HelperClient.displayParticles(entity ,
 			message.particleType, message.strength);
 	}
 	
