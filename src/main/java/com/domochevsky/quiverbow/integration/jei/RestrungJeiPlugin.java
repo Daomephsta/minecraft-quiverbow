0e597cdd491523fa5c99dc99f99dd12de7bd2f6f
Daomephsta <dtnilfisk@gmail.com>
2020-11-30 04:03:18 +1300

    Direct players to craft the incomplete ERA with a JEI info page

--- src/main/java/com/domochevsky/quiverbow/integration/jei/RestrungJeiPlugin.java
+++ src/main/java/com/domochevsky/quiverbow/integration/jei/RestrungJeiPlugin.java
@@ -4,14 +4,18 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 import com.domochevsky.quiverbow.ammo.ReloadSpecificationRegistry;
+import com.domochevsky.quiverbow.items.ItemRegistry;
 import com.google.common.collect.Streams;
 
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.ingredients.VanillaTypes;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 import mezz.jei.api.recipe.IStackHelper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 @JEIPlugin
 public class RestrungJeiPlugin implements IModPlugin
@@ -19,12 +23,13 @@ public class RestrungJeiPlugin implements IModPlugin
     @Override
     public void register(IModRegistry registry)
     {
-
         IStackHelper stackHelper = registry.getJeiHelpers().getStackHelper();
         Collection<?> recipes = Streams.stream(ReloadSpecificationRegistry.INSTANCE.getSpecifications())
             .map(entry -> new RecipeLoadAmmoCategory.Wrapper(entry.getKey(), entry.getValue(), stackHelper))
             .collect(Collectors.toList());
         registry.addRecipes(recipes, RecipeLoadAmmoCategory.ID);
+        registry.addIngredientInfo(new ItemStack(ItemRegistry.ENDER_RAIL_ACCELERATOR), VanillaTypes.ITEM,
+            ItemRegistry.ENDER_RAIL_ACCELERATOR.getUnlocalizedName() + ".jei_description");
     }
 
     @Override
